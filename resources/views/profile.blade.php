088f542423d032ecdb5342d441b03bfa3d909a1f
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-03-13 21:13:27 +0700

    first commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Profile</h1>
+@stop
+
+@section('content')
+    <p>Syaikhul Shafwan R</p><br>
+    <p></p>@include('footer')
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> </script>
+@stop
